e8f8cc56f82222eab8ad3142341578c0055c7220
TECH_NINJA06 <telangharsh06@gmail.com>
2025-01-27 22:22:11 +0530

    Chal Jaa Sim Sim

---
+++ exp3/Increment.asm
@@ -0,0 +1,62 @@
+section .data
+    msg db 'Enter a number: ', 0
+    result_msg db 'Next 4 numbers are: ', 0
+    newline db 0x0A, 0
+    num_input resb 4 
+
+section .bss
+    num resb 1  
+
+section .text
+    global _start
+
+_start:
+    mov eax, 4        
+    mov ebx, 1     
+    mov ecx, msg       
+    mov edx, 17    
+    int 80h
+
+    mov eax, 3      
+    mov ebx, 0    
+    mov ecx, num_input  
+    mov edx, 4    
+    int 80h
+
+    mov eax, [num_input]
+    sub al, '0'
+    mov [num], al     
+
+    mov eax, 4   
+    mov ebx, 1       
+    mov ecx, result_msg  
+    mov edx, 22      
+    int 80h
+
+    mov ecx, 4        
+    mov al, [num]    
+
+next_num:
+    inc al          
+    add al, '0'      
+    mov [num], al      
+
+    mov eax, 4         
+    mov ebx, 1     
+    lea ecx, [num]      
+    mov edx, 1       
+    int 80h
+
+    sub al, '0'       
+    loop next_num    
+
+    mov eax, 4      
+    mov ebx, 1          
+    mov ecx, newline   
+    mov edx, 1      
+    int 80h
+
+
+    mov eax, 1   
+    mov ebx, 0   
+    int 80h
