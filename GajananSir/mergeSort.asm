3b261c2911f762432cecbc4bcec9c804ed403b11
Harsh Telang <telangharsh06@gmail.com>
2025-02-26 21:48:14 +0530

    Update mergeSort.asm

--- GajananSir/mergeSort.asm
+++ GajananSir/mergeSort.asm
@@ -10,13 +10,12 @@ section .data
 
 section .bss
     num resb 2
-    temp_array resb array_len  ; Temporary array for merging
+    temp_array resb array_len
 
 section .text
     global _start
 
 _start:
-    ; Print original array
     mov eax, 4
     mov ebx, 1
     mov ecx, msg1
@@ -25,12 +24,10 @@ _start:
     
     call print_array
     
-    ; Call merge sort
-    mov eax, 0                ; left index
-    mov ebx, array_len - 1    ; right index
+    mov eax, 0
+    mov ebx, array_len - 1
     call merge_sort
     
-    ; Print sorted array
     mov eax, 4
     mov ebx, 1
     mov ecx, msg2
@@ -39,60 +36,44 @@ _start:
     
     call print_array
     
-    ; Exit program
     mov eax, 1
     mov ebx, 0
     int 80h
 
 merge_sort:
-    ; Parameters:
-    ; eax = left index
-    ; ebx = right index
-    
     push ebp
     mov ebp, esp
     
-    ; Check if left < right
     cmp eax, ebx
     jge merge_sort_end
     
-    ; Calculate middle point
     mov ecx, eax
     add ecx, ebx
-    shr ecx, 1            ; middle = (left + right) / 2
+    shr ecx, 1
     
-    ; Save registers
     push eax
     push ebx
     push ecx
     
-    ; Sort left half
-    ; eax is already left
-    mov ebx, ecx         ; right = middle
+    mov ebx, ecx
     call merge_sort
     
-    ; Restore registers
     pop ecx
     pop ebx
     pop eax
     
-    ; Save registers again
     push eax
     push ebx
     push ecx
     
-    ; Sort right half
     mov eax, ecx
-    inc eax              ; left = middle + 1
-    ; ebx is already right
+    inc eax
     call merge_sort
     
-    ; Restore registers
-    pop ecx              ; middle
-    pop ebx              ; right
-    pop eax              ; left
+    pop ecx
+    pop ebx
+    pop eax
     
-    ; Merge the sorted halves
     push eax
     push ebx
     push ecx
@@ -104,21 +85,14 @@ merge_sort_end:
     ret
 
 merge:
-    ; Parameters on stack:
-    ; [ebp+8]  = middle
-    ; [ebp+12] = right
-    ; [ebp+16] = left
-    
     push ebp
     mov ebp, esp
     
-    ; Initialize indices
-    mov esi, [ebp+16]    ; i = left
-    mov edi, [ebp+8]     ; middle
-    inc edi              ; j = middle + 1
-    mov ecx, [ebp+16]    ; k = left (position in temp array)
+    mov esi, [ebp+16]
+    mov edi, [ebp+8]
+    inc edi
+    mov ecx, [ebp+16]
     
-    ; Copy elements to temp array
 copy_to_temp:
     mov al, [array + ecx]
     mov [temp_array + ecx], al
@@ -126,17 +100,15 @@ copy_to_temp:
     cmp ecx, [ebp+12]
     jle copy_to_temp
     
-    mov ecx, [ebp+16]    ; Reset k to left
+    mov ecx, [ebp+16]
     
 merge_loop:
-    ; Check if we've exhausted left or right half
-    cmp esi, [ebp+8]     ; if i > middle
+    cmp esi, [ebp+8]
     jg copy_remaining_right
     
-    cmp edi, [ebp+12]    ; if j > right
+    cmp edi, [ebp+12]
     jg copy_remaining_left
     
-    ; Compare elements
     mov al, [temp_array + esi]
     cmp al, [temp_array + edi]
     jle copy_from_left
@@ -178,17 +150,14 @@ merge_end:
     pop ebp
     ret
 
-; Function to print array
 print_array:
-    mov ecx, 0            ; Array index
+    mov ecx, 0
 
 print_loop:
-    ; Convert number to ASCII
     mov al, [array + ecx]
     add al, '0'
     mov [num], al
     
-    ; Print number
     push ecx
     mov eax, 4
     mov ebx, 1
@@ -196,7 +165,6 @@ print_loop:
     mov edx, 1
     int 80h
     
-    ; Print space
     mov eax, 4
     mov ebx, 1
     mov ecx, space
@@ -208,7 +176,6 @@ print_loop:
     cmp ecx, array_len
     jl print_loop
     
-    ; Print newline
     mov eax, 4
     mov ebx, 1
     mov ecx, newline
