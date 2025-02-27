371a02043405394b7f4495ee7fd2cdcb4afdbea5
Harsh Telang <telangharsh06@gmail.com>
2025-02-27 12:14:51 +0530

    Bihari Boy

--- exp6/factorial.asm
+++ exp6/factorial.asm
@@ -1,47 +1,64 @@
+section .data
+    msg db "Enter the number: ", 0
+
+
 section .bss
-    num resb 2         ; Reserve 2 bytes for the input number
-    result resw 1      ; Variable to store the result
+    num resb 2        
+    result resw 1      
 
 section .text
     global _start
 
 _start:
-    ; Read input from the user
-    mov eax, 3         ; syscall: sys_read
-    mov ebx, 0         ; file descriptor: stdin
-    mov ecx, num       ; buffer to store input
-    mov edx, 2         ; number of bytes to read
-    int 0x80           ; interrupt to invoke syscall
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, 18
+    int 80h
 
-    ; Convert input from ASCII to integer
+    mov eax, 3       
+    mov ebx, 0        
+    mov ecx, num     
+    mov edx, 2       
+    int 80h        
+
     movzx ecx, byte [num]
     sub ecx, '0'
+    
+    push ecx
+    call factorial
 
-    mov ax, 1          ; Initialize AX register to 1 (factorial result)
+    mov eax, 4       
+    mov ebx, 1         
+    mov ecx, result    
+    mov edx, 2       
+    int 80h           
+
+    mov eax, 1         
+    xor ebx, ebx       
+    int 80h           
+
+factorial:
+    push ebp
+    mov ebp, esp
+    
+    mov ecx, [ebp + 8]
+    mov ax, 1    
 
 factorial_loop:
-    cmp ecx, 1         ; Compare CX with 1
-    jle end_factorial  ; If CX <= 1, jump to end_factorial
-    mul cx             ; Multiply AX by CX
-    dec ecx            ; Decrement CX
-    jmp factorial_loop ; Repeat the loop
+    cmp ecx, 1        
+    jle end_factorial 
+    mul cx        
+    dec ecx        
+    jmp factorial_loop
+    ret 
 
 end_factorial:
-    mov [result], ax   ; Store the result in memory
-
-    ; Convert result to ASCII and print it
+    mov [result], ax  
     mov eax, [result]
     add eax, '0'
     mov [result], ax
-
-    ; Print the result
-    mov eax, 4         ; syscall: sys_write
-    mov ebx, 1         ; file descriptor: stdout
-    mov ecx, result    ; buffer to print
-    mov edx, 2         ; number of bytes to write
-    int 0x80           ; interrupt to invoke syscall
-
-    ; Exit the program
-    mov eax, 1         ; syscall: exit
-    xor ebx, ebx       ; status: 0
-    int 0x80           ; interrupt to invoke syscall
+    
+    mov esp, ebp
+    pop ebp
+    ret 
--- exp6/fibo.asm
+++ exp6/fibo.asm
@@ -16,26 +16,34 @@ section .text
     global _start
 
 _start:
-    push prompt
-    push prompt_len
-    call write
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, prompt
+    mov edx, prompt_len
+    int 80h
     
-    push n
-    push 2
-    call read
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, n
+    mov edx, 2
+    int 80h
     
-    push msg
-    push msg_len
-    call write
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, msg_len
+    int 80h
     
     movzx ecx, byte [n]
     sub ecx, '0'
     push ecx
     call fibonacci
     
-    push newline
-    push 1
-    call write
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, newline
+    mov edx, 1
+    int 80h
     
     mov eax, 1
     xor ebx, ebx
@@ -47,18 +55,22 @@ fibonacci:
     
     mov byte [num1], '0'
     mov byte [num2], '1'
-    mov ecx, [ebp + 8]    ; get n from stack
+    mov ecx, [ebp + 8]
 
 fib_loop:
     push ecx
     
-    push num1
-    push 1
-    call write
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, num1
+    mov edx, 1
+    int 80h
     
-    push space
-    push 1
-    call write
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, space
+    mov edx, 1
+    int 80h
 
     push num1
     push num2
@@ -75,45 +87,17 @@ fib_loop:
     
     mov esp, ebp
     pop ebp
-    ret 4
-
-write:
-    push ebp
-    mov ebp, esp
-    
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, [ebp + 12]   
-    mov edx, [ebp + 8]    
-    int 80h
-    
-    mov esp, ebp
-    pop ebp
-    ret 8
-
-read:
-    push ebp
-    mov ebp, esp
-    
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, [ebp + 12]   
-    mov edx, [ebp + 8]    
-    int 80h
-    
-    mov esp, ebp
-    pop ebp
-    ret 8
+    ret 
 
 add_nums:
     push ebp
     mov ebp, esp
     
-    mov eax, [ebp + 12]   
+    mov eax, [ebp + 12]
     movzx eax, byte [eax]
     sub al, '0'
     
-    mov ebx, [ebp + 8]   
+    mov ebx, [ebp + 8]
     movzx ebx, byte [ebx]
     sub bl, '0'
     
@@ -123,4 +107,4 @@ add_nums:
     
     mov esp, ebp
     pop ebp
-    ret 8
+    ret 
