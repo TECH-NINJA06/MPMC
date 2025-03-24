52b773a7794e4eb4f2792e45a851ecca962b0827
D-Justin-Dsouza <justindsouza80@gmail.com>
2025-03-24 07:35:55 +0530

    Sucker

--- exp8/sumEle.asm
+++ exp8/sumEle.asm
@@ -1,173 +1,97 @@
 section .data
-    prompt db "Enter number: ", 0
-    prompt_len equ $ - prompt
-    sum_msg db "Sum of elements is: ", 0
-    sum_len equ $ - sum_msg
-    newline db 10, 0
+    nl db "", 10
+    nllen equ $-nl
+
+    msg1 db 'Enter number of elements: '
+    msg1len equ $-msg1
+
+    msg2 db 'Enter elements'
+    msg2len equ $-msg2
+
+    msg3 db 'Sum: '
+    msg3len equ $-msg3
+
+    arr times 100 db 0
+
+%macro write 2
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, %1
+    mov edx, %2
+    int 80h
+%endmacro
+
+%macro read 2
+    mov eax, 3
+    mov ebx, 2
+    mov ecx, %1
+    mov edx, %2
+    int 80h
+%endmacro
+
+%macro endl 0
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, nl
+    mov edx, nllen
+    int 80h
+%endmacro
 
 section .bss
-    array resw 5        
-    num resb 10         ; Buffer for input
-    sum resw 1    
+    cnt resb 1
+    n resb 1
+    ele resb 1
+    sm resb 1
 
 section .text
     global _start
-
 _start:
-    mov word [sum], 0   ; Initialize sum to 0
-    mov ecx, 5          ; Loop counter for 5 elements
-    mov esi, array      ; Pointer to the array
+    write msg1, msg1len
+    read n, 2
+    mov byte [cnt], 0
+    mov esi, arr
+    write msg2, msg2len
+    endl
+    input:
+        read ele, 2
+        mov ebx, [ele]
+        mov [esi], ebx
+
+        inc esi
+        inc byte [cnt]
+
+        mov al, [cnt]
+        mov bl, [n]
+        sub bl, '0'
+
+        cmp al, bl
+        je exit
+        jmp input
     
-input_loop:
-    push ecx            ; Save loop counter
+    exit:
+    mov esi, arr
+    mov byte [cnt], 0
+    mov byte [sm], 0
     
-    ; Display prompt
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, prompt
-    mov edx, prompt_len
-    int 80h
+    fsum:
+        mov bl, [esi]
+        sub bl, '0'
+        add byte [sm], bl
+
+        inc esi
+        inc byte[cnt]
+
+        mov al, [cnt]
+        mov bl, [n]
+        sub bl, '0'
+        cmp al, bl
+        jl fsum
     
-    ; Read number input
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, num
-    mov edx, 10      
-    int 80h
-    
-    ; Convert ASCII input to integer
-    call atoi
-    mov ax, dx    
-    
-    ; Store the number in the array
-    mov [esi], ax      
-    
-    ; Add number to sum
-    add word [sum], ax
-    
-    add esi, 2         ; Move to the next array element
-    pop ecx            ; Restore counter
-    loop input_loop
-    
-    ; Display sum message
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, sum_msg
-    mov edx, sum_len
-    int 80h
-    
-    ; Print the sum
-    mov ax, [sum]
-    call print_number
-    
-    ; Print newline
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, newline
-    mov edx, 1
-    int 80h
-    
-    ; Exit
+    add byte[sm], '0'
+    write msg3, msg3len
+    write sm, 1
+    endl
+
     mov eax, 1
-    xor ebx, ebx
+    mov ebx, 0
     int 80h
-
-; ==========================
-; Function: print_number
-; Prints a signed integer in decimal
-; ==========================
-print_number:
-    push ax
-    push bx
-    push cx
-    push dx
-
-    mov cx, 0         ; Counter for digits
-    mov bx, 10        ; Divisor for conversion
-
-    ; Handle negative numbers
-    cmp ax, 0
-    jge print_loop
-    mov byte [num], '-'
-    
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, num
-    mov edx, 1
-    int 80h
-    
-    neg ax            ; Convert negative number to positive
-
-print_loop:
-    xor dx, dx
-    div bx            ; AX = AX / 10, remainder in DX
-    push dx           ; Store remainder
-    inc cx            ; Increment counter
-    test ax, ax       ; Check if quotient is 0
-    jnz print_loop  
-
-print_digits:
-    pop dx
-    add dl, '0'       ; Convert to ASCII
-    mov [num], dl
-    
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, num
-    mov edx, 1
-    int 80h
-    loop print_digits  ; Loop until all digits are printed
-
-    pop dx
-    pop cx
-    pop bx
-    pop ax
-    ret
-
-; ==========================
-; Function: atoi
-; Converts ASCII string to integer
-; ==========================
-atoi:
-    xor ax, ax 
-    xor dx, dx 
-    xor cx, cx 
-
-    mov esi, num
-
-    ; Check for negative sign
-    mov al, [esi]
-    cmp al, '-'
-    jne atoi_convert
-    inc esi  
-    inc cx    ; Set negative flag
-
-atoi_convert:
-    xor bx, bx  
-
-atoi_loop:
-    mov al, [esi]   
-    cmp al, 10      
-    je atoi_finish
-    cmp al, 0        
-    je atoi_finish
-    cmp al, '0'     
-    jb atoi_finish
-    cmp al, '9'      
-    ja atoi_finish
-
-    sub al, '0'      
-    imul bx, bx, 10 
-    add bx, ax      
-
-    inc esi        
-    jmp atoi_loop
-
-atoi_finish:
-    cmp cx, 0      
-    je atoi_store_result
-    neg bx         ; If negative, negate the result
-
-atoi_store_result:
-    mov dx, bx    ; Store final result in DX
-    ret
